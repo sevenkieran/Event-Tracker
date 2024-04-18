3f1e9ccef1203edaa36794ad09db3948a14d0907
bmongird <bmongird@villanova.edu>
2024-04-18 13:07:01 -0400

    end of class

--- Event-Tracker/src/App.jsx
+++ Event-Tracker/src/App.jsx
@@ -2,11 +2,7 @@ import { useState } from "react";
 import "./index.css";
 
 function App() {
-  return (
-    <div className=" bg-slate-600 h-screen">
-      <h1 className="text-3xl font-bold underline">Event Tracker</h1>
-    </div>
-  );
+  return <div className="body-background"></div>;
 }
 
 export default App;
