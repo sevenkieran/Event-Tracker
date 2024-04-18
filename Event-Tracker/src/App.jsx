a8a047b462b7cac54ba59a7fd66c3c0918ca7ad1
Kieran Seven <sevenkieran@gmail.com>
2024-04-18 12:45:44 -0400

    testing

--- Event-Tracker/src/App.jsx
+++ Event-Tracker/src/App.jsx
@@ -2,7 +2,11 @@ import { useState } from "react";
 import "./index.css";
 
 function App() {
-  return <div className="bg-background h-screen"></div>;
+  return (
+    <div className=" bg-slate-600 h-screen">
+      <h1 className="text-3xl font-bold underline">Event Tracker</h1>
+    </div>
+  );
 }
 
 export default App;
