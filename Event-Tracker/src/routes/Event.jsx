20d67c74965106c0cc0aec82dcf5acaf05bdcd8b
bmongird <bmongird@villanova.edu>
2024-04-23 12:32:24 -0400

    set up routing

---
+++ Event-Tracker/src/routes/Event.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Event() {
+    return (
+        <div>
+            <h2>This is where the event will go</h2>
+        </div>
+    );
+}
+export default Event;
