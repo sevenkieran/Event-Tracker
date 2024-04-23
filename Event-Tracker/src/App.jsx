a8fbb832292a5231b17d21d5abb0389038f1102a
bmongird <bmongird@villanova.edu>
2024-04-23 12:53:26 -0400

    added routing functionality

--- Event-Tracker/src/App.jsx
+++ Event-Tracker/src/App.jsx
@@ -2,17 +2,28 @@ import React, { Component } from "react"
 import { useState } from "react";
 import "./index.css";
 import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import HomePage from "./routes/HomePage";
 import Event from "./routes/Event";
 
+//Currently not rendering a homepage or event. need to fix!
+
 function App() {
+
   return (
     <Router>
       <ul>
         <li>
-          <Link to="/Event">This is where the event will be displayed</Link>
+          <Link to="/HomePage">This is home</Link>
+        </li>
+        <li>
+          <Link to="/Event">Test</Link>
         </li>
       </ul>
       <Routes>
+          <Route
+              path="/HomePage"
+              element={<HomePage />}>
+          </Route>
           <Route
               path="/Event"
               element={<Event />}>
--- Event-Tracker/src/routes/Event.jsx
+++ Event-Tracker/src/routes/Event.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 function Event() {
+
     return (
         <div>
             <h2>This is where the event will go</h2>
