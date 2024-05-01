6b284e81d9a33e511e3aeb7cfebe143dc6eea7ed
bmongird <bmongird@villanova.edu>
2024-05-01 16:35:19 -0400

    added home button

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,11 @@ import { BackButton } from "./BackButton";
 
 function App() {
   return (
-    <div>
+    <div className="bg-slate-700">
       <Router>
+          <Link to="/HomePage" className="mx-4 text-white border-input hover:bg-slate-900 hover:text-accent-foreground inline-flex items-center justify-center whitespace-nowrap rounded-md text-lg font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 px-1 ease-out duration-500">
+            Home
+          </Link>
         <Routes>
           <Route path="/HomePage" element={<HomePage />}></Route>
           <Route path="/Event/*" element={<Event />}></Route>
