c7ad654b66278ed1b1e80c230d08d86528396fe9
bmongird <bmongird@villanova.edu>
2024-05-01 16:13:45 -0400

    resolving conflicts

--- src/routes/HomePage.jsx
+++ src/routes/HomePage.jsx
@@ -2,26 +2,6 @@ import { React, useState } from "react";
 import { Link, BrowserRouter as Router, Route } from "react-router-dom";
 import "../index.css";
 
-/*function HomePage() {
-  //This is where the API call should go
-  //const [events, setEvents] = useState();
-  //setEvents([1, 2, 3, 4, 5]);
-  //Events should be set using setEvents after data is fetched from the API using fetch().then()
-  //Then, we'll use events.map() to dynamically generate links to each event
-  const events = [1,2,3,4,5];
-
-  return (
-    <div>
-      <h1>This is the homepage</h1>
-      {events.map((num) => {
-        console.log(num);
-        return <Link to={`/event/${num}`}>{num} page</Link>;
-      })}
-    </div>
-  );
-}
-*/
-
 function App() {
   const [location, setLocation] = useState("");
   const [events, setEvents] = useState([]);
